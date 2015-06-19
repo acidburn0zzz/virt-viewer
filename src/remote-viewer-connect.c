795d499a0f0dd29c9620c078c7cd3609a8a9adf4
Daniel P. Berrange <berrange@redhat.com>
2015-06-19 09:37:23 +0100

    Add config.h include to remote-viewer-connect.c file

--- src/remote-viewer-connect.c
+++ src/remote-viewer-connect.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <config.h>
+
 #include "remote-viewer-connect.h"
 #include "virt-viewer-util.h"
 #include <glib/gi18n.h>
