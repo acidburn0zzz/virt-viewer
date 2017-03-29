4d08da967274675fb4df9ab28d811be31181d66f
Pavel Grunt <pgrunt@redhat.com>
2017-03-29 12:36:42 +0200

    Remove unused virt_viewer_app_set_zoom_level
    Acked-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

--- src/virt-viewer-app.h
+++ src/virt-viewer-app.h
@@ -68,7 +68,6 @@ void virt_viewer_app_free_connect_info(VirtViewerApp *self);
 gboolean virt_viewer_app_create_session(VirtViewerApp *self, const gchar *type, GError **error);
 gboolean virt_viewer_app_activate(VirtViewerApp *self, GError **error);
 gboolean virt_viewer_app_initial_connect(VirtViewerApp *self, GError **error);
-void virt_viewer_app_set_zoom_level(VirtViewerApp *self, gint zoom_level);
 gboolean virt_viewer_app_get_direct(VirtViewerApp *self);
 void virt_viewer_app_set_direct(VirtViewerApp *self, gboolean direct);
 void virt_viewer_app_set_hotkeys(VirtViewerApp *self, const gchar *hotkeys);
