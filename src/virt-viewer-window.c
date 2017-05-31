acidburn0zzz/virt-viewer

commit cc455b7f916110d7cfae6b7af753349e070c9494

window: Replace autoDrawer with native Gtk widgets
GtkRevealer was intrudced in Gtk+ 3.10 and, combined with Gtk Overlay
(intoduced in Gtk+ 3.2), can provide a more sustainably implementation
of the AutoDrawer functionality.

This approach is completely based on the approach taken by virt-manager:
https://github.com/virt-manager/virt-manager/commit/dc05600324f6b9a82b68581fc0a9c145f9889ce9

Resolves: https://bugs.freedesktop.org/show_bug.cgi?id=94495

Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
Acked-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

src/virt-viewer-window.c
@@ -41,7 +41,7 @@
 #include "virt-viewer-session.h"
 #include "virt-viewer-app.h"
 #include "virt-viewer-util.h"
-#include "view/autoDrawer.h"
+#include "virt-viewer-timed-revealer.h"
 
 #define ZOOM_STEP 10
 
@@ -91,13 +91,13 @@ struct _VirtViewerWindowPrivate {
 
     GtkBuilder *builder;
     GtkWidget *window;
-    GtkWidget *layout;
     GtkWidget *toolbar;
     GtkWidget *toolbar_usb_device_selection;
     GtkWidget *toolbar_send_key;
     GtkAccelGroup *accel_group;
     VirtViewerNotebook *notebook;
     VirtViewerDisplay *display;
+    VirtViewerTimedRevealer *revealer;
 
     gboolean accel_enabled;
     GValue accel_setting;
@@ -187,6 +187,8 @@ virt_viewer_window_dispose (GObject *object)
         g_object_unref(priv->builder);
         priv->builder = NULL;
     }
+
+    g_clear_object(&priv->revealer);
 
     for (it = priv->accel_list ; it != NULL ; it = it->next) {
         g_object_unref(G_OBJECT(it->data));
@@ -305,6 +307,8 @@ virt_viewer_window_init (VirtViewerWindow *self)
     g_value_init(&priv->accel_setting, G_TYPE_STRING);
 
     priv->notebook = virt_viewer_notebook_new();
+    gtk_widget_show(GTK_WIDGET(priv->notebook));
+
     priv->builder = virt_viewer_util_load_ui("virt-viewer.ui");
 
     gtk_widget_set_sensitive(GTK_WIDGET(gtk_builder_get_object(self->priv->builder, "menu-send")), FALSE);
@@ -335,7 +339,7 @@ virt_viewer_window_init (VirtViewerWindow *self)
     vbox = GTK_WIDGET(gtk_builder_get_object(priv->builder, "viewer-box"));
     virt_viewer_window_toolbar_setup(self);
 
-    gtk_box_pack_end(GTK_BOX(vbox), priv->layout, TRUE, TRUE, 0);
+    gtk_box_pack_end(GTK_BOX(vbox), GTK_WIDGET(priv->notebook), TRUE, TRUE, 0);
     gdk_rgba_parse(&color, "black");
     /* FIXME:
      * This method has been deprecated in 3.16.
@@ -344,7 +348,7 @@ virt_viewer_window_init (VirtViewerWindow *self)
      * For the bug report about this deprecated function, please, see:
      * https://bugs.freedesktop.org/show_bug.cgi?id=94276
      */
-    gtk_widget_override_background_color(priv->layout, GTK_STATE_FLAG_NORMAL, &color);
+    gtk_widget_override_background_color(GTK_WIDGET(priv->notebook), GTK_STATE_FLAG_NORMAL, &color);
 
     priv->window = GTK_WIDGET(gtk_builder_get_object(priv->builder, "viewer"));
     gtk_window_add_accel_group(GTK_WINDOW(priv->window), priv->accel_group);
@@ -481,7 +485,7 @@ virt_viewer_window_leave_fullscreen(VirtViewerWindow *self)
         virt_viewer_display_set_monitor(priv->display, -1);
         virt_viewer_display_set_fullscreen(priv->display, FALSE);
     }
-    ViewAutoDrawer_SetActive(VIEW_AUTODRAWER(priv->layout), FALSE);
+    virt_viewer_timed_revealer_force_reveal(priv->revealer, FALSE);
     gtk_widget_show(menu);
     gtk_widget_hide(priv->toolbar);
     gtk_widget_set_size_request(priv->window, -1, -1);
@@ -518,8 +522,7 @@ virt_viewer_window_enter_fullscreen(VirtViewerWindow *self, gint monitor)
     virt_viewer_window_menu_fullscreen_set_active(self, TRUE);
     gtk_widget_hide(menu);
     gtk_widget_show(priv->toolbar);
-    ViewAutoDrawer_SetActive(VIEW_AUTODRAWER(priv->layout), TRUE);
-    ViewAutoDrawer_Close(VIEW_AUTODRAWER(priv->layout));
+    virt_viewer_timed_revealer_force_reveal(priv->revealer, TRUE);
 
     if (priv->display) {
         virt_viewer_display_set_monitor(priv->display, monitor);
@@ -1064,6 +1067,7 @@ static void
 virt_viewer_window_toolbar_setup(VirtViewerWindow *self)
 {
     GtkWidget *button;
+    GtkWidget *overlay;
     VirtViewerWindowPrivate *priv = self->priv;
 
     priv->toolbar = g_object_ref(gtk_toolbar_new());
@@ -1109,16 +1113,10 @@ virt_viewer_window_toolbar_setup(VirtViewerWindow *self)
     gtk_toolbar_insert(GTK_TOOLBAR(priv->toolbar), GTK_TOOL_ITEM(button), 0);
     g_signal_connect(button, "clicked", G_CALLBACK(virt_viewer_window_toolbar_leave_fullscreen), self);
 
-    priv->layout = ViewAutoDrawer_New();
-
-    ViewAutoDrawer_SetActive(VIEW_AUTODRAWER(priv->layout), FALSE);
-    ViewOvBox_SetOver(VIEW_OV_BOX(priv->layout), priv->toolbar);
-    ViewOvBox_SetUnder(VIEW_OV_BOX(priv->layout), GTK_WIDGET(priv->notebook));
-    ViewAutoDrawer_SetOffset(VIEW_AUTODRAWER(priv->layout), -1);
-    ViewAutoDrawer_SetFill(VIEW_AUTODRAWER(priv->layout), FALSE);
-    ViewAutoDrawer_SetOverlapPixels(VIEW_AUTODRAWER(priv->layout), 1);
-    ViewAutoDrawer_SetNoOverlapPixels(VIEW_AUTODRAWER(priv->layout), 0);
-    gtk_widget_show(priv->layout);
+    priv->revealer = virt_viewer_timed_revealer_new(priv->toolbar);
+    overlay = GTK_WIDGET(gtk_builder_get_object(priv->builder, "viewer-overlay"));
+    gtk_overlay_add_overlay(GTK_OVERLAY(overlay),
+                            virt_viewer_timed_revealer_get_overlay_widget(priv->revealer));
 }
 
 VirtViewerNotebook*
@@ -1356,9 +1354,7 @@ virt_viewer_window_enable_kiosk(VirtViewerWindow *self)
     g_return_if_fail(VIRT_VIEWER_IS_WINDOW(self));
     priv = self->priv;
 
-    ViewOvBox_SetOver(VIEW_OV_BOX(priv->layout), gtk_drawing_area_new());
-    ViewAutoDrawer_SetActive(VIEW_AUTODRAWER(priv->layout), FALSE);
-    ViewAutoDrawer_SetOverlapPixels(VIEW_AUTODRAWER(priv->layout), 0);
+    virt_viewer_timed_revealer_force_reveal(priv->revealer, FALSE);
 
     /* You probably also want X11 Option "DontVTSwitch" "true" */
     /* and perhaps more distro/desktop-specific options */

src/Makefile.am
@@ -68,12 +68,8 @@ libvirt_viewer_la_SOURCES =					\
 	virt-viewer-window.c				\
 	virt-viewer-vm-connection.h			\
 	virt-viewer-vm-connection.c			\
-	view/autoDrawer.c				\
-	view/autoDrawer.h				\
-	view/drawer.c					\
-	view/drawer.h					\
-	view/ovBox.c					\
-	view/ovBox.h					\
+	virt-viewer-timed-revealer.c \
+	virt-viewer-timed-revealer.h \
 	$(NULL)
 
 if HAVE_GTK_VNC

src/view/autoDrawer.h
@@ -1,91 +1,0 @@
-/* *************************************************************************
- * Copyright (c) 2005 VMware Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * *************************************************************************/
-
-/*
- * autoDrawer.h --
- *
- *      Declarations for the ViewAutoDrawer GTK+ widget.
- */
-
-
-#ifndef LIBVIEW_AUTODRAWER_H
-#define LIBVIEW_AUTODRAWER_H
-
-
-#include "drawer.h"
-
-
-#define VIEW_TYPE_AUTODRAWER            (ViewAutoDrawer_GetType())
-#define VIEW_AUTODRAWER(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), VIEW_TYPE_AUTODRAWER, ViewAutoDrawer))
-#define VIEW_AUTODRAWER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), VIEW_TYPE_AUTODRAWER, ViewAutoDrawerClass))
-#define VIEW_IS_AUTODRAWER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), VIEW_TYPE_AUTODRAWER))
-#define VIEW_IS_AUTODRAWER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), VIEW_TYPE_AUTODRAWER))
-#define VIEW_AUTODRAWER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), VIEW_TYPE_AUTODRAWER, ViewAutoDrawerClass))
-
-typedef struct _ViewAutoDrawerPrivate ViewAutoDrawerPrivate;
-
-typedef struct _ViewAutoDrawer {
-   /* Must come first. */
-   ViewDrawer parent;
-
-   /* Private. */
-   ViewAutoDrawerPrivate *priv;
-} ViewAutoDrawer;
-
-typedef struct _ViewAutoDrawerClass {
-   /* Must come first. */
-   ViewDrawerClass parent;
-
-   /* Padding for future expansion */
-   void (*_view_reserved0)(void);
-   void (*_view_reserved1)(void);
-   void (*_view_reserved2)(void);
-   void (*_view_reserved3)(void);
-} ViewAutoDrawerClass;
-
-
-G_BEGIN_DECLS
-
-
-GType ViewAutoDrawer_GetType(void);
-
-GtkWidget *ViewAutoDrawer_New(void);
-
-void ViewAutoDrawer_SetSlideDelay(ViewAutoDrawer *that, guint delay);
-void ViewAutoDrawer_SetOverlapPixels(ViewAutoDrawer *that, guint overlapPixels);
-void ViewAutoDrawer_SetNoOverlapPixels(ViewAutoDrawer *that, guint noOverlapPixels);
-
-void ViewAutoDrawer_SetActive(ViewAutoDrawer *that, gboolean active);
-
-void ViewAutoDrawer_SetPinned(ViewAutoDrawer *that, gboolean pinned);
-
-void ViewAutoDrawer_SetFill(ViewAutoDrawer *that, gboolean fill);
-
-void ViewAutoDrawer_SetOffset(ViewAutoDrawer *that, gint offset);
-
-void ViewAutoDrawer_Close(ViewAutoDrawer *that);
-
-G_END_DECLS
-
-
-#endif /* LIBVIEW_AUTODRAWER_H */

src/view/drawer.c
@@ -1,366 +1,0 @@
-/* *************************************************************************
- * Copyright (c) 2005 VMware, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * *************************************************************************/
-
-/*
- * drawer.c -
- *
- *      Implementation of a GTK+ drawer, i.e. a widget that opens and closes by
- *      sliding smoothly, at constant speed, over another one.
- */
-
-#include <config.h>
-
-#include "drawer.h"
-
-
-struct _ViewDrawerPrivate
-{
-   unsigned int period;
-   double step;
-   double goal;
-   struct {
-      gboolean pending;
-      guint id;
-   } timer;
-};
-
-#define VIEW_DRAWER_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), VIEW_TYPE_DRAWER, ViewDrawerPrivate))
-
-/* The unaltered parent class. */
-static ViewOvBoxClass *parentClass;
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawerInit --
- *
- *      Initialize a ViewDrawer.
- *
- * Results:
- *      None
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-static void
-ViewDrawerInit(GTypeInstance *instance, // IN
-               gpointer klass G_GNUC_UNUSED)          // Unused
-{
-   ViewDrawer *that;
-
-   that = VIEW_DRAWER(instance);
-   that->priv = VIEW_DRAWER_GET_PRIVATE(that);
-
-   that->priv->period = 10;
-   that->priv->step = 0.2;
-   that->priv->timer.pending = FALSE;
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawerFinalize --
- *
- *      "finalize" method of a ViewOvBox.
- *
- * Results:
- *      None
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-static void
-ViewDrawerFinalize(GObject *object) // IN
-{
-   ViewDrawer *that;
-   ViewDrawerPrivate *priv;
-
-   that = VIEW_DRAWER(object);
-   priv = that->priv;
-
-   if (priv->timer.pending) {
-      g_source_remove(priv->timer.id);
-      priv->timer.pending = FALSE;
-   }
-
-   G_OBJECT_CLASS(parentClass)->finalize(object);
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawerClassInit --
- *
- *      Initialize the ViewDrawerClass.
- *
- * Results:
- *      None
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-static void
-ViewDrawerClassInit(gpointer klass) // IN
-{
-   GObjectClass *objectClass = G_OBJECT_CLASS(klass);
-
-   parentClass = g_type_class_peek_parent(klass);
-
-   objectClass->finalize = ViewDrawerFinalize;
-
-   g_type_class_add_private(klass, sizeof(ViewDrawerPrivate));
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawer_GetType --
- *
- *      Get the (memoized) GType of the ViewDrawer GTK+ object.
- *
- * Results:
- *      The GType
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-GType
-ViewDrawer_GetType(void)
-{
-   static GType type = 0;
-
-   if (type == 0) {
-      static const GTypeInfo info = {
-         sizeof (ViewDrawerClass),
-         NULL, /* BaseInit */
-         NULL, /* BaseFinalize */
-         (GClassInitFunc)ViewDrawerClassInit,
-         NULL,
-         NULL, /* Class Data */
-         sizeof (ViewDrawer),
-         0, /* n_preallocs */
-         (GInstanceInitFunc)ViewDrawerInit,
-         NULL
-      };
-
-      type = g_type_register_static(VIEW_TYPE_OV_BOX, "ViewDrawer", &info, 0);
-   }
-
-   return type;
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawer_New --
- *
- *      Create a new ViewDrawer GTK+ widget.
- *
- * Results:
- *      The widget
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-GtkWidget *
-ViewDrawer_New(void)
-{
-   ViewDrawer *that;
-
-   that = VIEW_DRAWER(g_object_new(VIEW_TYPE_DRAWER, NULL));
-
-   return GTK_WIDGET(that);
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawerOnTimer --
- *
- *      Timer callback of a ViewDrawer. If we have reached the goal, deschedule
- *      the timer. Otherwise make progress towards the goal, and keep the timer
- *      scheduled.
- *
- * Results:
- *      TRUE if the timer must be rescheduled.
- *      FALSE if the timer must not be rescheduled.
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-static gint
-ViewDrawerOnTimer(gpointer data) // IN
-{
-   ViewDrawer *that;
-   ViewDrawerPrivate *priv;
-   double fraction;
-
-   that = VIEW_DRAWER(data);
-   priv = that->priv;
-
-   fraction = ViewOvBox_GetFraction(VIEW_OV_BOX(that));
-   /*
-    * Comparing double values with '==' is most of the time a bad idea, due to
-    * the inexact representation of values in binary (see
-    * http://www2.hursley.ibm.com/decimal/decifaq1.html and http://boost.org/libs/test/doc/components/test_tools/floating_point_comparison.html).
-    * But in this particular case it is legitimate. --hpreg
-    */
-   if (priv->goal == fraction) {
-      return priv->timer.pending = FALSE;
-   }
-
-   ViewOvBox_SetFraction(VIEW_OV_BOX(that),
-                         priv->goal > fraction
-                            ? MIN(fraction + priv->step, priv->goal)
-                            : MAX(fraction - priv->step, priv->goal));
-   return TRUE;
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawer_SetSpeed --
- *
- *      Set the 'period' (in ms.) and 'step' properties of a ViewDrawer, which
- *      determine the speed and smoothness of the drawer's motion.
- *
- * Results:
- *      None
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-void
-ViewDrawer_SetSpeed(ViewDrawer *that,    // IN
-                    unsigned int period, // IN
-                    double step)         // IN
-{
-   ViewDrawerPrivate *priv;
-
-   g_return_if_fail(that != NULL);
-
-   priv = that->priv;
-
-   priv->period = period;
-   if (priv->timer.pending) {
-      g_source_remove(priv->timer.id);
-      priv->timer.id = g_timeout_add(priv->period, ViewDrawerOnTimer, that);
-   }
-   priv->step = step;
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawer_SetGoal --
- *
- *      Set the 'goal' property of a ViewDrawer, i.e. how much the drawer should
- *      be opened when it is done sliding.
- *
- * Results:
- *      None
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-void
-ViewDrawer_SetGoal(ViewDrawer *that, // IN
-                   double goal)      // IN
-{
-   ViewDrawerPrivate *priv;
-
-   g_return_if_fail(that != NULL);
-   g_return_if_fail(goal >= 0 && goal <= 1);
-
-   priv = that->priv;
-
-   priv->goal = goal;
-   if (priv->timer.pending == FALSE) {
-      priv->timer.id = g_timeout_add(priv->period, ViewDrawerOnTimer, that);
-      priv->timer.pending = TRUE;
-   }
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * ViewDrawer_GetCloseTime --
- *
- *    Get the approximate amount of time it will take for this drawer to
- *    open and close, in ms.
- *
- * Results:
- *      The time it takes to open or close the drawer.
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-int
-ViewDrawer_GetCloseTime(ViewDrawer *that)
-{
-   ViewDrawerPrivate *priv;
-
-   if (that == NULL) {
-      return 0;
-   }
-
-   priv = that->priv;
-
-   return priv->period * ((int)(1/priv->step) + 1);
-}

src/view/drawer.h
@@ -1,83 +1,0 @@
-/* *************************************************************************
- * Copyright (c) 2005 VMware, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * *************************************************************************/
-
-/*
- * drawer.h --
- *
- *      Declarations for the ViewDrawer GTK+ widget.
- */
-
-
-#ifndef LIBVIEW_DRAWER_H
-#define LIBVIEW_DRAWER_H
-
-
-#include "ovBox.h"
-
-
-#define VIEW_TYPE_DRAWER            (ViewDrawer_GetType())
-#define VIEW_DRAWER(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), VIEW_TYPE_DRAWER, ViewDrawer))
-#define VIEW_DRAWER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), VIEW_TYPE_DRAWER, ViewDrawerClass))
-#define VIEW_IS_DRAWER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), VIEW_TYPE_DRAWER))
-#define VIEW_IS_DRAWER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), VIEW_TYPE_DRAWER))
-#define VIEW_DRAWER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), VIEW_TYPE_DRAWER, ViewDrawerClass))
-
-typedef struct _ViewDrawerPrivate ViewDrawerPrivate;
-
-typedef struct _ViewDrawer {
-   /* Must come first. */
-   ViewOvBox parent;
-
-   /* Private. */
-   ViewDrawerPrivate *priv;
-} ViewDrawer;
-
-
-typedef struct _ViewDrawerClass {
-   /* Must come first. */
-   ViewOvBoxClass parent;
-
-   /* Padding for future expansion */
-   void (*_view_reserved0)(void);
-   void (*_view_reserved1)(void);
-   void (*_view_reserved2)(void);
-   void (*_view_reserved3)(void);
-} ViewDrawerClass;
-
-
-G_BEGIN_DECLS
-
-
-GType ViewDrawer_GetType(void);
-
-GtkWidget *ViewDrawer_New(void);
-
-void ViewDrawer_SetSpeed(ViewDrawer *that, unsigned int period, double step);
-void ViewDrawer_SetGoal(ViewDrawer *that, double fraction);
-int ViewDrawer_GetCloseTime(ViewDrawer *that);
-
-
-G_END_DECLS
-
-
-#endif /* LIBVIEW_DRAWER_H */

commit 8ab98eec203e0a113be7509e36d10c8511e186ac

window: Do not show fullscreen toolbar if in kiosk mode
Regression since commit cc455b7f916110d7cfae6b7af753349e070c9494.

Related: https://bugzilla.redhat.com/show_bug.cgi?id=1446161

Signed-off-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

src/virt-viewer-window.c
@@ -518,8 +518,11 @@ virt_viewer_window_enter_fullscreen(VirtViewerWindow *self, gint monitor)
 
     virt_viewer_window_menu_fullscreen_set_active(self, TRUE);
     gtk_widget_hide(menu);
-    gtk_widget_show(priv->toolbar);
-    virt_viewer_timed_revealer_force_reveal(priv->revealer, TRUE);
+
+    if (!priv->kiosk) {
+        gtk_widget_show(priv->toolbar);
+        virt_viewer_timed_revealer_force_reveal(priv->revealer, TRUE);
+    }
 
     if (priv->display) {
         virt_viewer_display_set_monitor(priv->display, monitor);