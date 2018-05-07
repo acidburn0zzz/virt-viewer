ffdae4a98dd452e25bbaa7c101cb5acc538baba5
Andrea Bolognani <abologna@redhat.com>
2018-05-07 16:40:30 +0200

    prepare-release: Drop references to Test::AutoBuild
    They are misleading, and no longer relevant anyway.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,14 +4,7 @@ set -e
 set -v
 
 test -n "$1" && RESULTS=$1 || RESULTS=results.log
-: ${AUTOBUILD_INSTALL_ROOT=$HOME/builder}
-
-# If run under the autobuilder, we must use --nodeps with rpmbuild;
-# but this can lead to odd error diagnosis for normal development.
-nodeps=
-if test "${AUTOBUILD_COUNTER+set}"; then
-  nodeps=--nodeps
-fi
+INSTALL_ROOT=$HOME/builder
 
 # Make things clean.
 test -f Makefile && make -k distclean || :
@@ -20,7 +13,7 @@ rm -rf build
 mkdir build
 cd build
 
-../autogen.sh --prefix=$AUTOBUILD_INSTALL_ROOT \
+../autogen.sh --prefix=$INSTALL_ROOT \
     --enable-compile-warnings=error
 
 make
@@ -40,19 +33,15 @@ rm -f *.tar.gz
 make dist
 
 if [ -z "$RELEASE_BUILD" ]; then
-  if [ -n "$AUTOBUILD_COUNTER" ]; then
-    EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
-  else
-    NOW=`date +"%s"`
-    EXTRA_RELEASE=".$USER$NOW"
-  fi
+  NOW=`date +"%s"`
+  EXTRA_RELEASE=".$USER$NOW"
 else
   EXTRA_RELEASE=""
 fi
 
 
 if [ -f /usr/bin/rpmbuild ]; then
-  rpmbuild $nodeps \
+  rpmbuild \
      --define "extra_release $EXTRA_RELEASE" \
      --define "_sourcedir `pwd`" \
      -ba --clean virt-viewer.spec
@@ -62,12 +51,12 @@ if [ -x /usr/bin/i686-w64-mingw32-gcc ]; then
   make distclean
 
   PKG_CONFIG_LIBDIR="/usr/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig:/usr/i686-w64-mingw32/sys-root/mingw/share/pkgconfig" \
-  PKG_CONFIG_PATH="$AUTOBUILD_INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
+  PKG_CONFIG_PATH="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
   CC="i686-w64-mingw32-gcc" \
   ../configure \
     --build=$(uname -m)-w64-linux \
     --host=i686-w64-mingw32 \
-    --prefix="$AUTOBUILD_INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw"
+    --prefix="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw"
 
   make
   make install
@@ -78,12 +67,12 @@ if [ -x /usr/bin/x86_64-w64-mingw32-gcc ]; then
   make distclean
 
   PKG_CONFIG_LIBDIR="/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig:/usr/x86_64-w64-mingw32/sys-root/mingw/share/pkgconfig" \
-  PKG_CONFIG_PATH="$AUTOBUILD_INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
+  PKG_CONFIG_PATH="$INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
   CC="x86_64-w64-mingw32-gcc" \
   ../configure \
     --build=$(uname -m)-w64-linux \
     --host=x86_64-w64-mingw32 \
-    --prefix="$AUTOBUILD_INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw"
+    --prefix="$INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw"
 
   make
   make install
@@ -91,7 +80,7 @@ fi
 
 if test -x /usr/bin/i686-w64-mingw32-gcc && test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
   if [ -f /usr/bin/rpmbuild ]; then
-    rpmbuild $nodeps \
+    rpmbuild \
        --define "extra_release $EXTRA_RELEASE" \
        --define "_sourcedir `pwd`" \
        -ba --clean mingw-virt-viewer.spec
